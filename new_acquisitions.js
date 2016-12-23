7c637b6b17eaf5b7de0f7f1d1615f00858f5a73a
Silvio Bindella <silvio.bindella@usi.ch>
2016-12-23 15:22:56 +0100

    removed local specificities from JS

--- new_acquisitions.js
+++ new_acquisitions.js
@@ -9,45 +9,76 @@ var customParams = {
 	bulkSize: '100' /* (string) amount of records to retrieve from primo to check against bookcovers existence */
 };
 
-// get interface language
-function isEng() {
-  return ($("body").hasClass("EXLCurrentLang_en_US")) ? true : false;
-}
-
-// wait for DOM
-$(function() {
-	// hide the static new books
-	$(".newacquisition").hide();
-});
-
-// call the php interface to retrieve 4 new acquisitions
+// call the php interface to retrieve 'desiredAmount' of new acquisitions
 $.post("/files/scripts/new-arrivals/arrival-interface.php", customParams,
     function(data){
-		$(".loader").hide();
+    	// do whatever you want with 'data' ...
+
+    	// you can parse and prettify
 		var obj = JSON.parse(data);
 		var pretty = JSON.stringify(obj, null, 2);
+		console.log(pretty)
+		/* Sample output:
+		[
+		  {
+		    "lds14": "ed. by Teresa Stoppani ... [et al.]",
+		    "title": "This thing called theory",
+		    "publisher": "London : Routledge, 2017",
+		    "format": "316 p.",
+		    "language": {
+		      "it": "inglese",
+		      "en": "English"
+		    },
+		    "isbn": "978-1-138-22299-1",
+		    "coverUrl": "http://books.google.com/books/content?id=IXOVDAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+		    "coverEngine": "Google Books",
+		    "permalink": "http://lumen.sbu.usi.ch/arc:Collezioni della biblioteca:arc_aleph000842721"
+		  },
+		  {
+		    "lds14": "Mihye An",
+		    "title": "Atlas of fantastic infrastructures : an intimate look at media architecture",
+		    "publisher": "Basel : Birkhäuser, 2016",
+		    "format": "392 p. : ill.",
+		    "language": {
+		      "it": "inglese",
+		      "en": "English"
+		    },
+		    "isbn": "978-3-0356-0628-7",
+		    "coverUrl": "http://books.google.com/books/content?id=qCpKrgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+		    "coverEngine": "Google Books",
+		    "permalink": "http://lumen.sbu.usi.ch/arc:Collezioni della biblioteca:arc_aleph000842678"
+		  },
+		  {
+		    "lds14": "Kate Hill",
+		    "title": "Culture and class in English public museums, 1850-1914",
+		    "publisher": "London : Routledge, 2016",
+		    "format": "174 p. : ill.",
+		    "language": {
+		      "it": "inglese",
+		      "en": "English"
+		    },
+		    "isbn": "978-0-7546-0432-7",
+		    "coverUrl": "http://covers.openlibrary.org/b/ISBN/9780754604327-L.jpg?default=false",
+		    "coverEngine": "Open Library",
+		    "permalink": "http://lumen.sbu.usi.ch/arc:Collezioni della biblioteca:arc_aleph000841182"
+		  },
+		  {
+		    "lds14": "[Hrsg.: Heinz Wirz]",
+		    "title": "Dreier Frenzel",
+		    "publisher": "Luzern : Quart Verlag, 2016",
+		    "format": "45 p. : ill.",
+		    "language": {
+		      "it": "tedesco",
+		      "en": "German"
+		    },
+		    "isbn": "978-3-03761-124-1",
+		    "coverUrl": "http://books.google.com/books/content?id=AcgHjwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+		    "coverEngine": "Google Books",
+		    "permalink": "http://lumen.sbu.usi.ch/arc:Collezioni della biblioteca:arc_aleph000842255"
+		  }
+		]
+		*/
 
-		var divs = $(".newacquisition");
-		for (var i = 0; i < obj.length; i++) {
-			$(divs[i]).find("img").attr("src", obj[i].coverUrl);
-			$(divs[i]).find(".newacquisition-title").text(obj[i].title);
-			if (obj[i].lds14) {
-				$(divs[i]).find(".newacquisition-responsibility").text(obj[i].lds14);
-			} else {
-				$(divs[i]).find(".newacquisition-responsibility").text("");
-			}
-			$(divs[i]).find(".newacquisition-publisher").text(obj[i].publisher);
-			$(divs[i]).find(".newacquisition-format").text(obj[i].format);
-			if (!isEng()) {
-				$(divs[i]).find(".newacquisition-language").text(obj[i].language.it);
-			} else {
-				$(divs[i]).find(".newacquisition-language").text(obj[i].language.en);
-			}
-			$(divs[i]).find(".newacquisition-isbn").text(obj[i].isbn);
-			$(divs[i]).find(".newacquisition-linktoprimo").attr("href", obj[i].permalink);
-		}
-		// show the updated new books
-		$(".newacquisition").show();
 }).fail(function(){
       console.log("error");
 });
